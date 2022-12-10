9525f24836530b5716f7e2679aa5cf8289b0ecb3
Daniel <danielfg1990@gmail.com>
2022-12-10 17:09:30 +0100

    small improvements

--- scrape.js
+++ scrape.js
@@ -1,14 +1,20 @@
 const fs = require('fs')
 const CardMarketScraper = require('./src/card-market-scraper')
 
-const fileBuffer = fs.readFileSync('input.txt')
-const fileText = fileBuffer.toString()
-const cardUrls = fileText.split('\n')
-console.log('cardUrls', cardUrls)
+async function scrape() {
+    // Get card urls from file input
+    const fileBuffer = fs.readFileSync('input.txt')
+    const fileText = fileBuffer.toString()
+    const cardUrls = fileText.split('\n')
+    console.log('cardUrls', cardUrls)
 
-CardMarketScraper.scrapeCardArticles(cardUrls).then(result => {
+    // Launch the scraper process passing an array of card urls
+    const result = await CardMarketScraper.scrapeCardArticles(cardUrls)
     console.log('CardMarkerScraper.scrapeCardArticles result', result)
 
+    // Generate a json file with the scraping result
     if (!fs.existsSync('result')) fs.mkdirSync('result')
     fs.writeFileSync('result/' + Date.now() + '.json', JSON.stringify(result, null, 2))
-})
+}
+
+scrape()
