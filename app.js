89b2c43af4ee31bababdc11406d7201cc2a21a89
Dukebot <danielfg1990@gmail.com>
2022-12-30 13:02:19 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,26 +5,23 @@ const bodyParser = require('body-parser')
 const Config = require('./src/config')
 const Controller = require('./src/controller')
 
-App(Controller, Config.port)
+const app = express()
+const port = Config.port
 
-function App(controllerFunction, port) {
-    const app = express()
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-    app.use(cors())
-    app.use(bodyParser.urlencoded({ extended: false }))
-    app.use(bodyParser.json())
-    
-    controllerFunction(app)
-    
-    app.use(function (req, res, next) {
-        res.status(404).send({ 
-            error: true, 
-            status: 404, 
-            message: 'URL not found' 
-        })
+Controller(app)
+
+app.use(function (req, res, next) {
+    res.status(404).send({ 
+        error: true, 
+        status: 404, 
+        message: 'URL not found' 
     })
-    
-    app.listen(port, () => {
-        console.log("The server is initialized at port:", port)
-    })
-}
+})
+
+app.listen(port, () => {
+    console.log("The server is initialized at port:", port)
+})
